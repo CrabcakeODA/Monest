8b73ea6c5c26c1b104d5a97b8c865adf0245aa50
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-01-07 14:32:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss";
 
 import Home from "views/pages/Home.jsx";
+import About from "views/pages/About/About.jsx"
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -40,6 +41,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact render={props => <Home {...props} />} />
+        <Route path="/about" exact render={props => <About {...props} />} />
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
