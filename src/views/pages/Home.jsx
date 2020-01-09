db072fad76b070c2ada25e05dec5191c5dff2db1
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-01-08 19:23:35 -0500

    Update Home.jsx

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -54,21 +54,22 @@ const items = [
   {
     src: "assets/img/bg/bg-1.jpeg",
     altText: 'bg-1.jpeg',
-    caption: 'Slide 1'
+    caption: 'We Lock lowest mortgage rate today'
   },
   {
     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
     altText: 'bg-2.jpeg',
-    caption: 'Slide 2'
+    caption: 'Learning your benefits and suitable plans as a first time buyer or new immigrant'
   },
   {
     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
     altText: 'bg-3.jpeg',
-    caption: 'Slide 3'
+    caption: 'Want to look for how you can afford your mortgage?'
   }
 ];
 
 const CarsouelImg = styled.img`
+  object-fit:cover;
   width: 100vw;
   max-height: 100vh;
 `
@@ -128,7 +129,7 @@ class Home extends React.Component {
         key={item.src}
       >
         <CarsouelImg width='100%' height='80%' src={require(`assets/img/bg/${item.altText}`)} alt={item.altText} />
-        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        <CarouselCaption className="align-items-center justify-content-center m-0 p-0" captionHeader={item.caption} />
       </CarouselItem>
     );
 
@@ -151,16 +152,16 @@ class Home extends React.Component {
               </div>
               <Container className="d-flex p-0 m-0">
                 <div className="p-0 m-0">
-                      <Carousel
-                        activeIndex={this.state.activeIndex}
-                        next={this.nextCarouselPage}
-                        previous={this.previousCarouselPage}
-                      >
-                        <CarouselIndicators items={items} activeIndex={this.state.activeIndex} onClickHandler={this.goToCarouselPage} />
-                        {slides}
-                        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previousCarouselPage} />
-                        <CarouselControl direction="next" directionText="Next" onClickHandler={this.nextCarouselPage} />
-                      </Carousel>
+                  <Carousel
+                    activeIndex={this.state.activeIndex}
+                    next={this.nextCarouselPage}
+                    previous={this.previousCarouselPage}
+                  >
+                    <CarouselIndicators items={items} activeIndex={this.state.activeIndex} onClickHandler={this.goToCarouselPage} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previousCarouselPage} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={this.nextCarouselPage} />
+                  </Carousel>
                 </div>
               </Container>
               {/* SVG separator */}
