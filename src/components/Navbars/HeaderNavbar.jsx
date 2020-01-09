8da43ea625e950b935793de226ddf2537f512e2d
annie <anniey9@hotmail.com>
2020-01-09 16:40:30 -0500

    navbar

--- src/components/Navbars/HeaderNavbar.jsx
+++ src/components/Navbars/HeaderNavbar.jsx
@@ -18,49 +18,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // JavaScript plugin that hides or shows a component based on your scroll
-import Headroom from "headroom.js";
+// import Headroom from "headroom.js";
 // reactstrap components
 import {
-  Button,
+  // Button,
   UncontrolledCollapse,
   DropdownMenu,
   DropdownItem,
   DropdownToggle,
   UncontrolledDropdown,
-  Media,
+  // Media,
   NavbarBrand,
   Navbar,
-  NavItem,
-  NavLink,
+  // NavItem,
+  // NavLink,
   Nav,
   Container,
   Row,
   Col,
-  UncontrolledTooltip
+  // UncontrolledTooltip
 } from "reactstrap";
 
+import "scss/HeaderNavbar.scss";
+
 class HeaderNavbar extends React.Component {
-  componentDidMount() {
-    let headroom = new Headroom(document.getElementById("navbar-main"));
-    // initialise
-    headroom.init();
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen:false,
+      isOpenMortgage: false,
+      isOpenCalculator: false,
+      isOpenAbout: false,
+    }
   }
+
   render() {
     return (
       <>
         <header className="header-global">
           <Navbar
-            className="navbar-main navbar-transparent navbar-light headroom"
+            className="navbar-main navbar-transparent navbar-dark headroom"
             expand="lg"
             id="navbar-main"
           >
             <Container>
               <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
                 <img
-                  alt="..."
-                  src={require("assets/img/brand/argon-react-white.png")}
+                  alt="Logo Imgae"
+                  // src={require("assets/img/brand/argon-react-white.png")}
                 />
               </NavbarBrand>
+              {/* logo icon */}
               <button className="navbar-toggler" id="navbar_global">
                 <span className="navbar-toggler-icon" />
               </button>
@@ -70,8 +79,8 @@ class HeaderNavbar extends React.Component {
                     <Col className="collapse-brand" xs="6">
                       <Link to="/">
                         <img
-                          alt="..."
-                          src={require("assets/img/brand/argon-react.png")}
+                          alt="Logo + Monest"
+                          // src={require("assets/img/brand/argon-react.png")}
                         />
                       </Link>
                     </Col>
@@ -83,178 +92,120 @@ class HeaderNavbar extends React.Component {
                     </Col>
                   </Row>
                 </div>
-                <Nav className="navbar-nav-hover align-items-lg-center" navbar>
-                  <UncontrolledDropdown nav>
+
+                
+                <Nav className=" menu navbar-nav-hover align-items-lg-center" navbar>
+                  <div className="menu-nav">
+                    <Link to="/" className="menu-item nav-link-inner--text">Home</Link>
+                  </div>
+
+                {/* mortgage  */}
+                <div className="menu-nav">
+                <Link to="/mortgage" className=" menu-item nav-link-inner--text">Mortgage</Link>
+                <UncontrolledDropdown nav className="mobile-dropdown">
                     <DropdownToggle nav>
-                      <i className="ni ni-ui-04 d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Components</span>
+                      <span className="btn-icon btn btn-primary" onClick={()=>this.setState({ isOpenMortgage: !this.state.isOpenMortgage, isOpen:this.state.isOpen  })}>
+                        { 
+                          this.state.isOpenMortgage ? <i className="fa fa-minus" /> : <i className="fa fa-plus" />
+                          // this.state.isOpen ? <i className="fa fa-minus" /> : <i className="fa fa-plus" />
+                        }
+                      </span>
                     </DropdownToggle>
-                    <DropdownMenu className="dropdown-menu-xl">
-                      <div className="dropdown-menu-inner">
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/overview?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                            <i className="ni ni-spaceship" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h6 className="heading text-primary mb-md-1">
-                              Getting started
-                            </h6>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Learn how to use Argon compiling Scss, change
-                              brand colors and more.
-                            </p>
-                          </Media>
-                        </Media>
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/colors?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-                            <i className="ni ni-palette" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h6 className="heading text-primary mb-md-1">
-                              Foundation
-                            </h6>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Learn more about colors, typography, icons and the
-                              grid system we used for Argon.
-                            </p>
-                          </Media>
-                        </Media>
-                        <Media
-                          className="d-flex align-items-center"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alert?ref=adsr-navbar"
-                          target="_blank"
-                        >
-                          <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                            <i className="ni ni-ui-04" />
-                          </div>
-                          <Media body className="ml-3">
-                            <h5 className="heading text-warning mb-md-1">
-                              Components
-                            </h5>
-                            <p className="description d-none d-md-inline-block mb-0">
-                              Browse our 50 beautiful handcrafted components
-                              offered in the Free version.
-                            </p>
-                          </Media>
-                        </Media>
-                      </div>
+                    <DropdownMenu className="mobile-dropdown-menu">
+                      <DropdownItem to="/mortgage/first-time-home-buyer" tag={Link}>
+                      First-Time Home Buyer
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/investment-properties" tag={Link}>
+                      Investment Properties
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/new-to-Canada" tag={Link}>
+                      New to Canada
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/low-income-education-centre" tag={Link}>
+                      Low income Education Centre
+                      </DropdownItem>
                     </DropdownMenu>
-                  </UncontrolledDropdown>
-                  <UncontrolledDropdown nav>
+                </UncontrolledDropdown>
+                  </div>
+
+                  {/* Calculator  */}
+                  <div className="menu-nav">
+                  <Link to="/calculator" className="menu-item nav-link-inner--text">Calculator</Link>
+                  <UncontrolledDropdown nav className="mobile-dropdown">
                     <DropdownToggle nav>
-                      <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Examples</span>
+                      <span className="btn-icon btn btn-primary" onClick={()=>this.setState({ isOpenCalculator: !this.state.isOpenCalculator })}>
+                        { 
+                          this.state.isOpenCalculator ? <i className="fa fa-minus" /> : <i className="fa fa-plus" />
+                        }
+                      </span>
                     </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem to="/landing-page" tag={Link}>
-                        Landing
+                    <DropdownMenu className="mobile-dropdown-menu">
+                      <DropdownItem to="/calculator/mortgage-affordability-calculator" tag={Link}>
+                      Mortgage Affordability Calculator
                       </DropdownItem>
-                      <DropdownItem to="/profile-page" tag={Link}>
-                        Profile
-                      </DropdownItem>
-                      <DropdownItem to="/login-page" tag={Link}>
-                        Login
-                      </DropdownItem>
-                      <DropdownItem to="/register-page" tag={Link}>
-                        Register
+                      <DropdownItem to="/calculator/mortgage-payment-calculator" tag={Link}>
+                      Mortgage Payment Calculator
                       </DropdownItem>
                     </DropdownMenu>
                   </UncontrolledDropdown>
-                </Nav>
-                <Nav className="align-items-lg-center ml-lg-auto" navbar>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://www.facebook.com/creativetim"
-                      id="tooltip333589074"
-                      target="_blank"
-                    >
-                      <i className="fa fa-facebook-square" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Facebook
+                  </div>
+
+                  {/* About */}
+                  <div className="menu-nav">
+                  <Link to="/about" className="menu-item nav-link-inner--text">About</Link>
+                  <UncontrolledDropdown nav className="mobile-dropdown">             
+                    <DropdownToggle nav>
+                    <span className="btn-icon btn btn-primary" 
+                      onClick={
+                        ()=>this.setState({ isOpenAbout: !this.state.isOpenAbout })
+                      }>
+
+                        { 
+                          this.state.isOpenAbout 
+                          ? <i className="fa fa-minus" /> 
+                          : <i className="fa fa-plus" />
+                        }
+
                       </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip333589074">
-                      Like us on Facebook
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://www.instagram.com/creativetimofficial"
-                      id="tooltip356693867"
-                      target="_blank"
-                    >
-                      <i className="fa fa-instagram" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Instagram
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip356693867">
-                      Follow us on Instagram
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://twitter.com/creativetim"
-                      id="tooltip184698705"
-                      target="_blank"
-                    >
-                      <i className="fa fa-twitter-square" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Twitter
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip184698705">
-                      Follow us on Twitter
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className="nav-link-icon"
-                      href="https://github.com/creativetimofficial/argon-design-system-react"
-                      id="tooltip112445449"
-                      target="_blank"
-                    >
-                      <i className="fa fa-github" />
-                      <span className="nav-link-inner--text d-lg-none ml-2">
-                        Github
-                      </span>
-                    </NavLink>
-                    <UncontrolledTooltip delay={0} target="tooltip112445449">
-                      Star us on Github
-                    </UncontrolledTooltip>
-                  </NavItem>
-                  <NavItem className="d-none d-lg-block ml-lg-4">
-                    <Button
-                      className="btn-neutral btn-icon"
-                      color="default"
-                      href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-navbar"
-                      target="_blank"
-                    >
-                      <span className="btn-inner--icon">
-                        <i className="fa fa-cloud-download mr-2" />
-                      </span>
-                      <span className="nav-link-inner--text ml-1">
-                        Download
-                      </span>
-                    </Button>
-                  </NavItem>
+                    </DropdownToggle>
+                    <DropdownMenu className="mobile-dropdown-menu">
+                      <DropdownItem to="/mortgage/first-time-home-buyer" tag={Link}>
+                      Our Story
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/investment-properties" tag={Link}>
+                      Agents
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/new-to-Canada" tag={Link}>
+                      Referral Program
+                      </DropdownItem>
+                      <DropdownItem to="/mortgage/low-income-education-centre" tag={Link}>
+                      Mortgage Insight
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  </div>
+
+                  {/* Contact */}
+                  <div className="menu-nav">
+                    <Link to="/contact" className="menu-item nav-link-inner--text">Contact</Link>
+                  </div>
+
+                  {/* 中文 */}
+                  <div className="menu-nav">
+                  <i className="fa fa-minus" />
+                  </div>
+
+                  {/* English */}
+                  <div className="menu-nav">
+                  <i className="fa fa-minus" />
+                  </div>
                 </Nav>
               </UncontrolledCollapse>
             </Container>
           </Navbar>
         </header>
-      </>
+        </>
+      
     );
   }
 }
